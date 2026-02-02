777048822bca45a6836e6d33c305783cb2a0a0b0
Yash Yadav <179575411+yashyadav-4@users.noreply.github.com>
2026-02-02 11:08:27 +0530

    setting up project

--- CPPro/server/index.js
+++ CPPro/server/index.js
@@ -3,7 +3,9 @@ const express= require('express');
 const app= express();
 const port=5000;
 
-
+app.get('/api/test' , (req, res)=>{
+    res.json({ message :"backend is working"});
+})
 
 app.listen(port , ()=>{
     console.log('Server is live at : ' , port);
